52106ad55ad593b4819f483b0dbd95f0a82fa625
Crasel <paulo.parra.lozoya@gmail.com>
2021-05-01 12:12:17 +0200

    Syntax error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);
 
-let lastestDate = Date.now() - 3600000 * 10;
+let lastestDate = Date.now() - 3600000 * 8;
 
 
 function revisarEnlaces()
