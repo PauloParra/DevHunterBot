5082b0624547f3c8cd56d0e8ec72cc4799d46107
Crasel <paulo.parra.lozoya@gmail.com>
2021-05-07 14:43:07 +0200

    mas pruebas

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ const talents = require("./talenthacker");
 
 const client = new Discord.Client();
 
+// HACE QUE EL BOT PUEDA SER INICIADO BUSCANDO EL TOKEN
 client.login(process.env.BOT_TOKEN);
 
-let lastestDate = Date.now(); 
+// OBTIENE LA HORA ACTUAL DEL SERVIDOR DE HEROKU
+let lastestDate = Date.now();
 
-  function revisarEnlaces()
-  {
-    // BORRAR AL COMPROBAR QUE ENTRA SIEMPRE
-    console.log("Reviso los enlaces");
+function revisarEnlaces() {
+  // BORRAR AL COMPROBAR QUE ENTRA SIEMPRE
+  console.log("Reviso los enlaces");
 
-    talents(lastestDate, 'https://talenthackers.net/spots/', '?rid=Jg7CFCYggrDn')
-    .then(result => {    
+  talents(lastestDate, 'https://talenthackers.net/spots/', '?rid=Jg7CFCYggrDn')
+    .then(result => {
       if (result.lastestDate) {
         lastestDate = result.lastestDate;
-        
-        for(let url of result.urls) {
+
+        for (let url of result.urls) {
           // client.channels.cache.get('801717547090051104').send(url);
           client.channels.cache.get('836477980267249726').send(url);
         }
-      }    
+      }
     })
     .catch(console.error);
-  }
+}
 
 
-  
-  client.on('ready', () => {
-    console.log(`Bot is ready ${client.user.tag}`);
-    revisarEnlaces(); 
-    // setInterval(revisarEnlaces, 3600000);
-    setInterval(revisarEnlaces, 60000);
+
+client.on('ready', () => {
+  console.log(`Bot is ready ${client.user.tag}`);
+  revisarEnlaces();
+  // setInterval(revisarEnlaces, 3600000);
+  setInterval(revisarEnlaces, 60000);
 
 
 
--- talenthacker.js
+++ talenthacker.js
@@ -1,7 +1,8 @@
 const request = require('request');
-
-function requestPromise(link) {
-    return new Promise((resolve, reject) => {
+ 
+function requestPromise(link)
+{
+    return new Promise( (resolve, reject) => {
         request(link, (err, res, body) => {
             if (!err) {
                 resolve(body);
@@ -12,44 +13,61 @@ function requestPromise(link) {
         });
     });
 }
-
+ 
+let mins = 0;
 module.exports = (date, link, affiliate) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise( async (resolve, reject) => {       
         try {
             let lastestDate;
             const urls = [];
-
-            const body = await requestPromise(link);
-
+ 
+            /*const body = await requestPromise(link);
+ 
             const script = body.match(/<script>window\.__INITIAL_STATE__=(.*?)<\/script>/);
-
+    
             const json = JSON.parse(script[1]);
-
-            const jobs = json.allJobs.splice(0, 5);
-
-            for (let job of jobs) {
-                const jobLink = `${link}${job.area}/${job.slug}`;
-                const now = new Date(new Date().toLocaleString("en-US", {timeZone: 'Europe/Madrid'}));
-                const jobBody = await requestPromise(jobLink);
-
+            
+            const jobs = json.allJobs.splice(0, 5);*/
+ 
+            const jobs = [
+                {
+                    body: 'Actualizado hace 1 horas',
+                    link: 'https://talenthackers.net/spots/Development/tech-leadcto-441'
+                },
+                {
+                    body: 'Actualizado hace 2 horas',
+                    link: 'https://talenthackers.net/spots/Digital/customer-success-manager-tech-427'
+                },
+                {
+                    body: 'Actualizado hace 3 horas',
+                    link: 'https://talenthackers.net/spots/SysOps/administrador-de-sistemas-439'
+                }
+            ];
+ 
+            for(let job of jobs) {
+                //const jobLink = `${link}${job.area}/${job.slug}`;
+                const jobLink = job.link;
+                const now = new Date();
+                //const jobBody = await requestPromise(jobLink);
+                const jobBody = job.body;
+ 
                 let actualizado = jobBody.match(/Actualizado hace (\d+) horas/);
-                if (actualizado) {
-                    console.log(`Actualizado hace ${actualizado[1]} horas. Ahora: ${new Date()}`);
-                    // const published = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours()) - actualizado[1] * 3600000;
-                    let published = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours()) - actualizado[1] * 3600000;
-
+                if (actualizado) {                                                    
+                    //const published = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours()) - actualizado[1] * 3600000;
+                    const published = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours()) - actualizado[1] * 3600000 + mins * 60000;
                     console.log(`${new Date(published)} > ${new Date(date)}`);
                     if (published > date) {
                         lastestDate = lastestDate ? Math.max(lastestDate, published) : published;
-                        urls.push(jobLink + affiliate);
+                        urls.push(jobLink+affiliate);
                     }
                 }
             }
-
-            resolve({ urls, lastestDate });
+ 
+            mins++;
+            resolve({urls, lastestDate});
         }
-        catch (error) {
+        catch(error){
             reject(error);
-        }
-    });
+        }        
+    });    
 }
